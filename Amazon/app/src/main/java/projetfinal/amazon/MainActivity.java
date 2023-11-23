56a6869f2857fb56170fc4a1185dffa1d2298beb
Yohan Chuet <202284927@cegepsherbrooke.qc.ca>
2023-11-22 20:52:00 -0500

    Page d'acceuil fonctionne

--- Amazon/app/src/main/java/projetfinal/amazon/MainActivity.java
+++ Amazon/app/src/main/java/projetfinal/amazon/MainActivity.java
@@ -2,7 +2,12 @@ package projetfinal.amazon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.confirmerBouton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = ((EditText) findViewById(R.id.nameEditText)).getText().toString();
+                String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+                if (DataBaseAChanger.isUserGood(name, password))
+                {
+                    Toast.makeText(MainActivity.this, "lesgo", Toast.LENGTH_SHORT).show();
+                    // open new window
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Wrong password or name: "  + name + " " + password , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
