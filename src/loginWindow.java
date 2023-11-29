7c7c5380750ec4949dd62dc06f6f746333d03db9
ThomasFortier <fortier-thomas@outlook.com>
2023-11-29 17:35:05 -0500

    loginWindow advanced

--- src/loginWindow.java
+++ src/loginWindow.java
@@ -1,15 +1,31 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class loginWindow {
+public class loginWindow extends JFrame{
     private JTextField userNameTextField;
     private JPasswordField passwordField1;
     private JButton loginButton;
     private JButton cancelButton;
+    private JPanel pnlMain;
 
 
     public loginWindow(){
 
+        setContentPane(pnlMain);
 
+        setTitle("Login Amazon");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(360,250);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
 
