2f1c58abc7c5ac45dcdecbef319d7a86af13a7fa
Yohan Chuet <202284927@cegepsherbrooke.qc.ca>
2023-11-29 17:10:03 -0500

    create class client and Article

---
+++ src/Article.java
@@ -0,0 +1,14 @@
+public class Article {
+    String name;
+    double price;
+    String pathImage;
+    int remainingQuantity;
+
+    public void Article(String _name, double _price, String _pathImage, int _remainingQuantity ) {
+        this.name = _name;
+        this.price = _price;
+        this.pathImage = _pathImage;
+        this.remainingQuantity = _remainingQuantity;
+    }
+
+}
