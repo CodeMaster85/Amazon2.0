9c421d2677985cfa3a3594bb0541b9afd1008e79
ThomasFortier <fortier-thomas@outlook.com>
2023-12-01 15:26:55 -0500

    userNameTextField key listener

--- src/createAccountWindow.java
+++ src/createAccountWindow.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,19 +26,17 @@ public class createAccountWindow extends JFrame {
     private JLabel errorJLabel2;
 
 
-
-    public createAccountWindow(){
+    public createAccountWindow() {
         setContentPane(pnlMain);
 
         setTitle("Create an account");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(400,600);
+        setSize(400, 600);
         setLocationRelativeTo(null);
 
         createAccountButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
 
                 ArrayList<JTextField> textField = new ArrayList<>();
                 textField.add(firstNameTextField);
@@ -42,14 +46,33 @@ public class createAccountWindow extends JFrame {
                 textField.add(passwordTextField);
                 textField.add(confirmPasswordTextField);
 
-                for( JTextField i : textField){
-                    if(i.getText().isEmpty()){
+                for (JTextField i : textField) {
+                    if (i.getText().isEmpty()) {
                         errorJLabel.setText("Error : Please complete all the tabs");
                     }
-                    if (!Arrays.equals(passwordTextField.getPassword(), confirmPasswordTextField.getPassword())){ //Merci André
+                    if (!Arrays.equals(passwordTextField.getPassword(), confirmPasswordTextField.getPassword())) { //Merci André
                         errorJLabel2.setText("Error : Passwords does not match");
                     }
                 }
+
+            }
+
+        });
+        usernameTextField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == ' ') {
+                    e.consume(); // Empêche l'ajout de l'espace dans le JTextField
+                }
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
             }
         });
         cancelButton.addActionListener(new ActionListener() {
