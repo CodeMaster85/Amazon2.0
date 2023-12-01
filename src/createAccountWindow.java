0f42d00229a2a691093cea115a67d49c43c6707c
ThomasFortier <fortier-thomas@outlook.com>
2023-12-01 13:11:03 -0500

    new GUI createAccountWindow

---
+++ src/createAccountWindow.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class createAccountWindow {
+    private JPanel pnlMain;
+    private JTextField nameTextField;
+    private JTextField lastNameTextField;
+    private JTextField usernameTextField;
+    private JTextField emailTextField;
+    private JTextField passwordTextField;
+    private JTextField confirmPassordTextField;
+    private JLabel errorJLabel;
+    private JButton createAccountButton;
+    private JButton cancelButton;
+
+}
--- src/loginWindow.java
+++ src/loginWindow.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class loginWindow extends JFrame{
     private JTextField userNameTextField;
@@ -8,6 +10,7 @@ public class loginWindow extends JFrame{
     private JButton loginButton;
     private JButton cancelButton;
     private JPanel pnlMain;
+    private JLabel createJLabel;
 
 
     public loginWindow(){
@@ -16,13 +19,47 @@ public class loginWindow extends JFrame{
 
         setTitle("Login Amazon");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(360,250);
+        setSize(400,250);
         setLocationRelativeTo(null);
         setVisible(true);
 
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                userNameTextField.setText("");
+                passwordField1.setText("");
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        createJLabel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                createJLabel.setText("Go on Walmart");
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
 
             }
         });
