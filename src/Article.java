a8f446c2fcf0b0c967a4697aa75c8a3b400e42bd
Yohan Chuet <202284927@cegepsherbrooke.qc.ca>
2023-12-11 16:28:10 -0500

    advanced principalWindow + Article

--- src/Article.java
+++ src/Article.java
@@ -1,13 +1,45 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Article {
     String name;
     double price;
     String pathImage; 
     int remainingQuantity;
 
-    public  Article(String _name, double _price, String _pathImage, int _remainingQuantity ) {
+    public Article(String _name, double _price, String _pathImage, int _remainingQuantity ) {
         this.name = _name;
         this.price = _price;
         this.pathImage = _pathImage;
         this.remainingQuantity = _remainingQuantity;
+    }
+
+    public static ArrayList<Article> SearchAllArticle()
+    {
+        ArrayList<Article> item = new ArrayList<>();
+
+        File file = new File("Amazone2.0\\src\\dataWish.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String ligne;
+            while ((ligne = br.readLine()) != null)
+            {
+                if (ligne.equals("Article"))
+                {
+                    String name = br.readLine();
+                    double price = Double.parseDouble(br.readLine());
+                    String pathImage = br.readLine();
+                    int remainingQuantity = Integer.parseInt(br.readLine());
+                    item.add(new Article(name, price, pathImage, remainingQuantity));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return item;
+
     }
 }
--- src/PrincipalWindow.java
+++ src/PrincipalWindow.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PrincipalWindow extends JFrame{
     private JPanel pnlMain;
@@ -7,10 +8,14 @@ public class PrincipalWindow extends JFrame{
     private JLabel acceuilButtonLabel;
     private JLabel cartJLabel;
     private JPanel menuJPanel;
+    private JPanel shopJPanel;
+    private JScrollBar scrollBar1;
 
     public PrincipalWindow()
     {
-        //this.pnlMain = new JPanel();
+        shopJPanel = new JPanel();
+        pnlMain = new JPanel();
+        pnlMain.setLayout(new BoxLayout(pnlMain, BoxLayout.Y_AXIS));
         setContentPane(pnlMain);
 
         setTitle("Login XYT Express");
@@ -20,13 +25,76 @@ public class PrincipalWindow extends JFrame{
 
 
         ImageIcon icon = new ImageIcon("Amazone2.0\\images\\logoXYT.png");
-
+        Image scaledImage = icon.getImage().getScaledInstance(61, 37, Image.SCALE_SMOOTH);
+        icon = new ImageIcon(scaledImage);
         acceuilButtonLabel.setIcon(icon);
 
 
-        // Rendre la fenêtre visible
+        ArrayList<Article> allArticle = Article.SearchAllArticle();
+
+        for (Article i : allArticle)
+        {
+            shopJPanel.add(createArticlePanel(i));
+        }
+
+
+        pnlMain.add(menuJPanel);
+        pnlMain.add(shopJPanel);
+
         setVisible(true);
 
 
     }
+
+//    private JPanel createArticlePanel(Article article) {
+//        JPanel articlePanel = new JPanel();
+//        articlePanel.setLayout(new GridLayout(1, 4));
+//
+//        // Ajouter les informations de l'article au panneau
+//        ImageIcon icon = new ImageIcon(article.pathImage);
+//        Image scaledImage = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+//        icon = new ImageIcon(scaledImage);
+//        JLabel pathImageLabel = new JLabel(icon);
+//
+//        JLabel nameLabel = new JLabel(article.name);
+//        JLabel priceLabel = new JLabel("Prix: " + article.price);
+//
+//        JLabel quantityLabel = new JLabel("Quantité restante: " + article.remainingQuantity);
+//
+//        articlePanel.add(nameLabel);
+//        articlePanel.add(priceLabel);
+//        articlePanel.add(pathImageLabel);
+//        articlePanel.add(quantityLabel);
+//
+//        return articlePanel;
+//    }
+private JPanel createArticlePanel(Article article) {
+    JPanel articlePanel = new JPanel(new BorderLayout());
+
+    // Ajouter l'image redimensionnée au centre
+    ImageIcon icon = new ImageIcon(article.pathImage);
+    Image scaledImage = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+    icon = new ImageIcon(scaledImage);
+    JLabel pathImageLabel = new JLabel(icon);
+    articlePanel.add(pathImageLabel, BorderLayout.CENTER);
+
+    // Créer un panneau pour le nom, le prix et la quantité
+    JPanel infoPanel = new JPanel();
+    infoPanel.setLayout(new GridLayout(3, 1));
+
+    // Ajouter le nom, le prix et la quantité au panneau d'informations
+    JLabel nameLabel = new JLabel(article.name);
+    JLabel priceLabel = new JLabel("Prix: $" + article.price);
+    JLabel quantityLabel = new JLabel("Quantité restante: " + article.remainingQuantity);
+
+    infoPanel.add(nameLabel);
+    infoPanel.add(priceLabel);
+    infoPanel.add(quantityLabel);
+
+    // Ajouter le panneau d'informations au côté est du panneau principal
+    articlePanel.add(infoPanel, BorderLayout.EAST);
+
+    return articlePanel;
+}
+
 }
